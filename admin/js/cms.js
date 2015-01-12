8691fec528415da86a7e87407a8ce2bd71bd4e45
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-01-12 10:42:12 -0800

    Added authentication and working on saving docs

--- admin/js/cms.js
+++ admin/js/cms.js
@@ -1,9 +1,17 @@
 "use strict";
 
+var Promise = Ember.RSVP.Promise;
+
 window.CMS = Ember.Application.create();
 CMS.deferReadiness();
 $.get("config.yml").then(function(data) {
   CMS.config = CMS.Config.create(jsyaml.safeLoad(data));
+  CMS.Repository.configure(CMS.config);
+  var token = localStorage.getItem("cms.token");
+  if (token) {
+    CMS.Repository.setToken(token);
+    CMS.loggedIn = true;
+  }
   CMS.advanceReadiness();
 });
 
@@ -17,20 +25,193 @@ CMS.Config = Ember.Object.extend({
   }
 });
 
-CMS.ApplicationView = Ember.View.extend({});
+CMS.Repository = (function() {
+  var base, branch;
+  var ENDPOINT = "https://api.github.com/";
+  var token = null;
+
+  function request(url, settings) {
+    return $.ajax(url, $.extend(true, {headers: {Authorization: "Bearer " + token}}, settings || {}));
+  };
+
+  function getBranch() {
+    return request(base+ "/branches/" + branch);
+  };
+
+  function getTree(sha) {
+    return sha ? request(base + "/git/trees/" + sha) : new Promise(function(resolve) { resolve({tree: []})});
+  };
+
+  function uploadBlob(file) {
+    return new Promise(function(resolve, reject) {
+      request(base + "/git/blobls", {
+        type: "POST",
+        data: {
+          content: file.base64 ? file.base64() : Base64.encode(file.content),
+          encoding: "base64"
+        }
+      }).then(function(response) {
+        file.sha = response.sha;
+        resolve(file);
+      }, function(err) {
+        reject(err);
+      });
+    });
+  };
+
+  function updateTree(sha, path, fileTree) {
+    return new Promise(function(resolve, reject) {
+      getTree(sha).then(function(tree) {
+        var obj, fileOrDir;
+        var update = [];
+        for (var i=0, len=tree.tree.length; i<len; i++) {
+          obj = tree.tree[i];
+          if (fileOrDir = fileTree[obj.path]) {
+            fileOrDir._added = true;
+            updates.push(
+              fileOrDir.file ?
+                {path: obj.path, mode: obj.mode, type: obj.type, sha: fileOrDir.sha} :
+                updateTree(obj.sha, obj.path, fileOrDir)
+            );
+          } else {
+            updates.push(updateTree(obj.sha, obj.path, fileOrDir));
+          }
+        }
+        for (filename in fileTree) {
+          fileOrDir = fileTree[filename];
+          if (fileOrDir._added) { continue; }
+          updates.push(
+            fileOrDir.file ?
+              {path: filename, mode: "100644", type: "blob", sha: fileOrDir.sha} :
+              updateTree(null, filename, fileOrDir)
+          );
+        }
+        Promise.all(updates).then(function(updates) {
+          request(base + "/git/trees", {
+            type: "POST",
+            data: {base_tree: sha, tree: updates}
+          }).then(function(response) {
+            resolve({path: path, mode: "040000", type: "tree", sha: response.sha});
+          }, function(err) {
+            reject(err);
+          })
+        });
+      })
+    });
+  };
+
+  return {
+    configure: function(config) { 
+      base = ENDPOINT + "repos/" + config.repo;
+      branch = config.branch;
+    },
+    setToken: function(newToken) { token = newToken; },
+    readFile: function(path) {
+      return request(base + "/contents/" + path, {
+        headers: {Accept: "application/vnd.github.VERSION.raw"},
+        data: {ref: branch}
+      });
+    },
+    updateFiles: function(options) {
+      return new Promise(function(resolve, reject) {
+        var file, filename, part, parts, subtree;
+        var fileTree = {};
+        var files = [];
+        for (var i=0, len=options.files.length; i<len; i++) {
+          file = options.files[i];
+          files.push(file.upload ? file : uploadBlob(file));
+          parts = file.path.split("/").filter(function(part) { return part; });
+          filename = parts.pop();
+          subtree = fileTree;
+          while (part = parts.shift()) {
+            subtree[part] = subtree[part] || {}
+            subtree = subtree[part];
+          }
+          subtree[filename] = file;
+          file.file = true;
+        }
+        Promise.all(files).then(function() {
+          getBranch().then(function(branchData) { 
+            updateTree(branchData.commit.sha, "/", fileTree).then(function(changeTree) {
+              request(base + "/git/commits", {
+                type: "POST",
+                data: {message: options.message, tree: changeTree.sha, parents: [branchData.commit.sha]}
+              }).then(function(response) {
+                request(base + "/git/refs/heads/" + branch, {
+                  type: "PATCH",
+                  data: {sha: response.sha}
+                }).then(function(ref) {
+                  resolve(ref);
+                }, function(err) { reject(err); });
+              }, function(err) { reject(err); });
+            }, function(err) { reject(err); });
+          }, function(err) { reject(err); });
+        }, function(err) { reject(err); });
+      });
+    }
+  }
+})();
+
 CMS.ApplicationController = Ember.Controller.extend({
   currentAction: "Dashboard"
 });
 
 CMS.Router.map(function() {
+  this.route("login"),
+  this.route("logout"),
   this.route("home", {path: "/"}),
-  this.resource('collections', {path: '/collections/:collection_id'}, function() {
-    this.route('new');
-  });
+  this.route("create", {path: "/collections/:collection_id"}),
+  this.route("edit", {path: "/collections/:collection_id/:slug"})
 });
 
-CMS.HomeRoute = Ember.Route.extend({
+CMS.LogoutRoute = Ember.Route.extend({
+  activate: function() {
+    CMS.loggedIn = false;
+    CMS.Repository.setToken(null);
+  }
+});
+
+CMS.LoginController = Ember.Controller.extend({
+ actions: {
+    login: function() {
+      netlify.configure({site_id: 'timespace.netlify.com'});
+      netlify.authenticate({provider: "github", scope: "repo"}, function(err, data) {
+        if (err) {
+          this.controller.set("error", err);
+          console.log(this);
+        } else {
+          localStorage.setItem("cms.token", data.token);
+          CMS.Repository.setToken(data.token);
+          CMS.loggedIn = true
+          this.transitionToRoute("home");
+        }
+      }.bind(this));
+    }
+  }
+})
+
+CMS.AuthenticatedRoute = Ember.Route.extend({
+  activate: function() {
+    if (!CMS.loggedIn) {
+      this.transitionTo("login");
+    }
+  }
+})
+
+CMS.HomeRoute = CMS.AuthenticatedRoute.extend({
   model: function() { return CMS.config; }
+});
+
+CMS.CreateRoute = CMS.AuthenticatedRoute.extend({});
+CMS.EditRoute = CMS.AuthenticatedRoute.extend({
+  model: function(params) {
+    return CMS.Entry.find(params.collection_id, params.slug);
+  }
+});
+CMS.EditController = Ember.Controller.extend({
+  collection: function() {
+    return this.get("model._collection");
+  }.property("model._collection")
 });
 
 CMS.Collection = Ember.Object.extend({
@@ -41,11 +222,43 @@ CMS.Collection = Ember.Object.extend({
       fields.push(CMS.Field.create(this.fields[i]));
     }
     this.fields = fields;
+  },
+  setEntry: function(entry) {
+    this.entry = entry;
+    for (var i=0, len=this.fields.length; i<len; i++) {
+      this.fields[i].set("value", entry[this.fields[i].name]);
+    }
   }
 });
 CMS.Collection.reopenClass({
   find: function(id) {
     return CMS.config.collections.filter(function(c) { return c.id == id})[0];
+  }
+});
+
+CMS.Entry = Ember.Object.extend({
+  _collection: null
+});
+CMS.Entry.reopenClass({
+  pathFor: function(collection, slug) {
+    return collection.folder + "/" + slug + ".md";
+  },
+  parseContent: function(content) {
+    var regexp = /^---\n([^]*?)\n---\n([^]*)$/;
+    var match = content.match(regexp);
+    var item = jsyaml.safeLoad(match[1]);
+    item.body = (match[2] || "").replace(/^\n+/, '');
+    return item;
+  },
+  find: function(collection_id, slug) {
+    var collection = CMS.Collection.find(collection_id);
+    return new Promise(function(resolve, reject){
+      CMS.Repository.readFile(CMS.Entry.pathFor(collection, slug)).then(function(content) {
+        var entry = CMS.Entry.create($.extend(CMS.Entry.parseContent(content), {_collection: collection}));
+        collection.setEntry(entry);
+        resolve(entry);
+      }.bind(this));
+    })
   }
 });
 
@@ -74,7 +287,11 @@ CMS.CmsExpandingTextareaComponent = Ember.TextArea.extend({
     if (this.element.scrollHeight > this.element.clientHeight) {
       this.element.style.height = this.element.scrollHeight + "px"
     }
-  }.observes("value")
+  }.observes("value"),
+
+  didInsertElement: function() {
+    this.valueChanged();
+  }
 })
 
 CMS.CmsListComponent = Ember.Component.extend({
